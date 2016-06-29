073a5434454423609352bf41d4b35b5414d7bf8e
Denise Williams <williamsde@outlook.com>
2016-06-29 08:28:28 -0400

    added instance variables and properties

---
+++ VirtualPet/Pet.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualPet
+{
+    class Pet
+    {
+        //instance variables
+        //Properties
+        //methods
+        //constructors
+    }
+}
